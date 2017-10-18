0dce0438d7c57611831c8ab4a14f9f2b5c72bf1f
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-18 10:04:46 -0400

    j update

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,34 +1,30 @@
-//import React
 import React from 'react';
-//es6 style below. then you can use Component instead of React.Component. it is the same as assigning React.Component to a variable and using that i.e. syntactic sugar:
+//es6 option below allows 'Component' instead of 'React.Component'. same as const Component = React.Component
   //import React, { Component } from 'react';
 
-// //create a new functional react component that produces HTML. this is 'dumb' or plain, as it just sends out some jsx when called.
-// const SearchBar = ()=> {
-//   return <input />; //this jsx will turn into a React.createElement call which depends on the import of React at the top.
-// };
-
-
-//create an es6 class based react component. It's just a javascript object until functionality added when extended.
+//create an es6 class based react component. It's just a plain js  object until extended.
 class SearchBar extends React.Component {
-  //State is an object used to record and react to user events, existing on all class based components (instances). When a component's state changes the component and its children re-render. First, initialize the state object...
-  //the constructor function performs setup inside of class
+  //State is an object used to record and react to user events, existing on all class-based components (instances). When a component's state changes, it re-renders. First, initialize the state object...
+
+  //the constructor function performs setup inside of a class
   constructor(props) {
     //call the parent method (from React.Component) with super
     super(props);
-    //Initialization: assign the state object to this.state . Here in the constructor function is the ONLY time you will use code like this to manipulate state. ( this.state= )
+    //Initialization: assign the state object to this.state  Here in the constructor function is the ONLY time to use this.state to MANIPULATE state.
     this.state = { term: '' };
   }
 
-  //define the render method on this class. all class based react components must have this. this is also the syntax for defining methods on a class.
+  //define render method. all class-based react components must have this. this is also the syntax for defining methods on a class.
   render() {
-    //commented out to conitnue with course
+    //commented out to continue with course
     //return <input onChange={this.onInputChange} />;
 
-    //outside of the constructor function set set state using this.setState
+    //outside of the constructor function MANIPULATE state using this.setState
     return (
       <div>
-        <input onChange={ event => this.setState({ term: event.target.value }) } />
+        <input
+          value={this.state.term}
+          onChange={ event => this.setState({ term: event.target.value }) } />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
-//example of using index.js , remember to only have one component per js file
-//---------------------------------------------------------------------------
-
-//import React and ReactDOM libraries
-import React from 'react';
+//import libraries
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-//import your personal code written in your js files
-//relative path refresher: ./ means start in the current directory
+import YTSearch from 'youtube-api-search';
+//importing personal code? use relative path with necessary ./ indicating starting directory
 import SearchBar from './components/search_bar';
-
-//declare constant for api key
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+//constants
 const API_KEY = 'AIzaSyD7PZ1yhWl3KdHG57spUvC2IkP6Z52iEAo';
 
-//create a new react component (class) that produces HTML
+//create a class-based (originally this was functional. but updated. hence the es5 example) react component that produces HTML. It just sends out jsx when called.
 //---es5 syntax
   // const App = function(){
-  //   return <div>poop</div>;
+  //   return <div>poop</div>; //this jsx will turn into a React.createElement call which depends on the import of React at the top.
   // };
-//---es6 syntax , block brackets and return keyword left in for clarity
-const App = ()=> {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-};
+//---es6 arrow function syntax
+class App extends Component {
+  //constructor function
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,//this will be changed when a video is clicked, see below.
+    };
+
+    YTSearch({key: API_KEY, term: 'dogs'}, (data)=> {
+      this.setState({
+        videos: data,
+        selectedVideo: data[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail videoElement={this.state.selectedVideo}/>
+
+        {/*this callback function passes to video_list then to _item where it receives an argument and calls back to here with it. */}
+        <VideoList
+          onVideoSelect={ (selectedVideo)=>{
+              this.setState({selectedVideo:selectedVideo});
+            } }
+          videos={this.state.videos}
+        />
+
+      </div>
+    );
+  }
+
+}
 
 //render the components html on the page
-ReactDOM.render(<App />, document.querySelector('.container')); //or you can
+ReactDOM.render(
+  <App />, document.querySelector('.container')
+); //or you can
   //also pass a regular jsx tag: <App></App>
